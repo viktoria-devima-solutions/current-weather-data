7b2ff0e5e3901636fff13581fe90c53aa9eda165
viktoria-devima-solutions <viktoria.m@devima.solutions>
2023-07-03 17:52:36 +0300

    Finish task

--- pages/index.js
+++ pages/index.js
@@ -1,115 +1,89 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import { useState, useEffect } from "react";
+import * as React from "react";
 
-export default function Home() {
+import TextField from "@mui/material/TextField";
+import { Typography } from "@mui/material";
+import Box from "@mui/material/Box";
+
+import { url, API_KEY, imageUrl } from "../api/constants";
+
+function Header({ title }) {
+  return <h1>{title ? title : "Default title"}</h1>;
+}
+
+export default function HomePage() {
+  const [search, setSeacrh] = useState("");
+  const [result, setResult] = useState(null);
+  const axios = require("axios");
+  axios.default.params = {};
+  axios.interceptors.request.use(
+    function (config) {
+      config.params["appid"] = API_KEY;
+      return config;
+    },
+    function (error) {
+      return Promise.reject(error);
+    }
+  );
+  useEffect(() => {
+    setResult(null);
+    axios
+      .get(`${url}`, {
+        params: {
+          q: search,
+          units: "metric",
+        },
+      })
+      .then(function (response) {
+        setResult(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, [search]);
+
+  function debounce(fn, delay) {
+    let timeoutId;
+    return (...args) => {
+      if (timeoutId) clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => fn(...args), delay);
+    };
+  }
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+    <Box sx={{ m: 2 }}>
+      <Typography variant="h3" component="h2">
+        Current weather data
+      </Typography>
+      <TextField
+        id="outlined-basic"
+        label="Location"
+        variant="outlined"
+        onChange={debounce((e) => {
+          setSeacrh(e.target.value);
+        }, 500)}
+      />
+      {result
+        ? result.data.weather.map((weather) => (
+            <Box
+              key={weather.id}
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+              }}
+            >
+              <div>
+                <Typography variant="h4" component="h2">
+                  {weather.main}
+                </Typography>
+                <Typography variant="h5" component="h2">
+                  {weather.description}
+                </Typography>
+              </div>
+              <img src={imageUrl + "/" + weather.icon + "@2x.png"} />
+            </Box>
+          ))
+        : null}
+    </Box>
+  );
 }
