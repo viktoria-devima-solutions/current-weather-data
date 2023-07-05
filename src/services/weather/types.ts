5c48e0aac67259323f94b63a859df2b27c337fc7
Viktoria Monastyrska <viktoria.m@devima.solutions>
2023-07-05 17:30:38 +0300

    refactor: interface IWeatherApiData structure

--- src/services/weather/types.ts
+++ src/services/weather/types.ts
@@ -1,47 +1,53 @@
+export interface IWeatherApiCoord {
+  lon: number;
+  lat: number;
+}
+export interface IWeatherApiWeather {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+}
+
+export interface IWeatherApiMain {
+  temp: number;
+  feels_like: number;
+  temp_min: number;
+  temp_max: number;
+  pressure: number;
+  humidity: number;
+  sea_level: number;
+  grnd_level: number;
+}
+export interface IWeatherApiWind {
+  speed: number;
+  deg: number;
+  gust: number;
+}
+export interface IWeatherApiRain {
+  "1h": number;
+}
+export interface IWeatherApiClouds {
+  all: number;
+}
+export interface IWeatherApiSys {
+  type: number;
+  id: number;
+  country: string;
+  sunrise: number;
+  sunset: number;
+}
 export interface IWeatherApiData {
-  coord: {
-    lon: number;
-    lat: number;
-  };
-  weather: [
-    {
-      id: number;
-      main: string;
-      description: string;
-      icon: number;
-    }
-  ];
+  coord: IWeatherApiCoord;
+  weather: IWeatherApiWeather[];
   base: string;
-  main: {
-    temp: number;
-    feels_like: number;
-    temp_min: number;
-    temp_max: number;
-    pressure: number;
-    humidity: number;
-    sea_level: number;
-    grnd_level: number;
-  };
+  main: IWeatherApiMain;
   visibility: number;
-  wind: {
-    speed: number;
-    deg: number;
-    gust: number;
-  };
-  rain: {
-    "1h": number;
-  };
-  clouds: {
-    all: number;
-  };
+  wind: IWeatherApiWind;
+  rain: IWeatherApiRain;
+  clouds: IWeatherApiClouds;
   dt: number;
-  sys: {
-    type: number;
-    id: number;
-    country: string;
-    sunrise: number;
-    sunset: number;
-  };
+  sys: IWeatherApiSys;
   timezone: number;
   id: number;
   name: string;
